0d48264f79f78233b818a3f8f4b5f401c5ff2bc6
Potenciano Angelyne <potenciano.angelyne.bernabe@gmail.com>
2025-11-05 17:02:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-898551163-48ee5",
+  "appId": "1:245187257689:web:3b82994cf60c5b2e91d848",
+  "apiKey": "AIzaSyBWbbzDyT_A-1VQz4-OzgSH3UbGVA40PEM",
+  "authDomain": "studio-898551163-48ee5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "245187257689"
+};
