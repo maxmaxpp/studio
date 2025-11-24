d76875ac662db91c85dd4bd0419f4b60c288ffb2
Potenciano Angelyne <potenciano.angelyne.bernabe@gmail.com>
2025-11-24 15:37:32 +0000

    bring it closer to header more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "@/components/sections/hero";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center pt-12">
+    <div className="flex flex-col items-center justify-center">
       <HeroSection />
     </div>
   );
