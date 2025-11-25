bcf314bc21a3b99307c283649f1ff48de2e1e149
Potenciano Angelyne <potenciano.angelyne.bernabe@gmail.com>
2025-11-25 05:03:43 +0000

    that's good. now adjust ONLY the cons-design.png up

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,7 +11,7 @@ export default function AboutPage() {
       <div className="absolute -top-24 -right-48 opacity-30 md:opacity-50 pointer-events-none">
         <Image src="/sun-design.png" alt="Sun design" width={600} height={600} />
       </div>
-      <div className="absolute -bottom-48 -left-48 opacity-30 md:opacity-50 pointer-events-none">
+      <div className="absolute -bottom-24 -left-48 opacity-30 md:opacity-50 pointer-events-none">
         <Image src="/cons-design.png" alt="Constellation design" width={500} height={500} />
       </div>
       <div className="container mx-auto relative">
