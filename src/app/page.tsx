0dbcdfdc6e39a953ed279598b898c15a640598eb
Potenciano Angelyne <potenciano.angelyne.bernabe@gmail.com>
2025-11-26 06:15:00 +0000

    now add testimonial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import HeroSection from "@/components/sections/hero";
+import TestimonialsSection from "@/components/sections/testimonials";
 import WorkflowSection from "@/components/sections/workflow";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center">
       <HeroSection />
       <WorkflowSection />
+      <TestimonialsSection />
     </div>
   );
 }
