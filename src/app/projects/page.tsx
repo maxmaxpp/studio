326bb35fc2ff4ca553d8656492392de6b03fb2ac
Potenciano Angelyne <potenciano.angelyne.bernabe@gmail.com>
2025-11-26 06:45:19 +0000

    now in projects, add a polka dots and a grainy texture

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -15,6 +15,11 @@ export default function ProjectsPage() {
   return (
     <div 
       className="relative h-[calc(100vh-8rem)] w-[calc(100vw)] -translate-x-1/2 left-1/2 overflow-hidden"
+       style={{
+            backgroundImage: 'radial-gradient(hsl(var(--border)) 1px, transparent 1px), url("data:image/svg+xml,%3Csvg viewBox=\'0 0 600 600\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cfilter id=\'noiseFilter\'%3E%3CfeTurbulence type=\'fractalNoise\' baseFrequency=\'0.65\' numOctaves=\'3\' stitchTiles=\'stitch\'/%3E%3C/filter%3E%3Crect width=\'100%25\' height=\'100%25\' filter=\'url(%23noiseFilter)\'/%3E%3C/svg%3E")',
+            backgroundSize: '16px 16px, auto',
+            backgroundBlendMode: 'overlay',
+        }}
     >
       <motion.div 
         initial={{ opacity: 1, y: 0 }}
